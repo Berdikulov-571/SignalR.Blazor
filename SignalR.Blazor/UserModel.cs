312f0c8b6ad4c4437f176ab3577fb67447ceaa3e
berdikulov-571 <mygithub571@gmail.com>
2023-12-15 19:38:23 +0500

    Model Added

---
+++ SignalR.Blazor/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.Blazor
+{
+    public class UserModel
+    {
+        public string ConnectionId { get; set; }
+        public string Message { get; set; }
+        public string UserName { get; set; }
+    }
+}
