4499f2669d928989172ad10733e60a78291a8516
berdikulov-571 <mygithub571@gmail.com>
2023-12-16 13:24:19 +0500

    ChatHub.cs

--- SignalR.Blazor/ChatHub.cs
+++ SignalR.Blazor/ChatHub.cs
@@ -27,5 +27,21 @@ namespace SignalR.Blazor
             await Clients.All.SendAsync("ReceiveMessage", user, message);
             messages.Add(new UserModel() { Message = message, UserName = user });
         }
+
+        public async Task SendToAllMessage(string user, string message)
+            => await Clients.All.SendAsync("ReceiveMessage", user, Context.ConnectionId, message);
+
+        public async Task SendToUserMessage(string id, string user, string message)
+        {
+            await Clients.Client(id).SendAsync("ReceiveMessageFromUser", user, message);
+            await Clients.Caller.SendAsync("ReceiveMessageFromUser", user, message);
+
+        }
+
+        public async Task SendToGroupMessage(string groupName, string user, string message)
+            => await Clients.Group(groupName).SendAsync("ReceiveMessageFromGroup", user, message);
+
+        public async Task JoinGroup(string groupName)
+            => await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
     }
 }
