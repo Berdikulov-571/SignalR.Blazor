74aa004579977565465ccf6eeac9eb7f40fa79f3
berdikulov-571 <mygithub571@gmail.com>
2023-12-16 12:55:49 +0500

    ChatHub

--- SignalR.Blazor/ChatHub.cs
+++ SignalR.Blazor/ChatHub.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 
 namespace SignalR.Blazor
 {
     public class ChatHub : Hub
     {
         private static List<UserModel> messages = new List<UserModel>();
-        
+
         private static Dictionary<string, string> _users = new Dictionary<string, string>();
 
         public async Task Start(string user, string message)
@@ -15,18 +14,18 @@ namespace SignalR.Blazor
 
             foreach (var i in messages)
             {
-                await Clients.Caller.SendAsync("History",i);
+                await Clients.Caller.SendAsync("History", i);
             }
 
             await Clients.Others.SendAsync("Other", user);
 
-            await Clients.Caller.SendAsync("Welcome",user);    
+            await Clients.Caller.SendAsync("Welcome", user);
         }
 
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
-            messages.Add(new UserModel() { Message = message, UserName = user});
+            messages.Add(new UserModel() { Message = message, UserName = user });
         }
     }
 }
